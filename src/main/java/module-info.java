410179c9b0a83486014cdfbf69750e52a20d214d
denzhe dzebu <dedzebujhb024@student.wethinkcode.co.za>
2025-05-19 09:59:19 +0200

    added hashing

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.denzhe.demo1 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires jbcrypt;
 
     opens org.denzhe.demo1 to javafx.fxml;
     exports org.denzhe.demo1;
--- src/main/java/org/denzhe/demo1/User.java
+++ src/main/java/org/denzhe/demo1/User.java
@@ -1,5 +1,5 @@
 package org.denzhe.demo1;
-
+import org.mindrot.jbcrypt.BCrypt;
 public class User{
 
     public static boolean login(String usnm, String psswrd){
@@ -8,5 +8,14 @@ public class User{
         String username = "admin";
         //todo: implement hashing and dbstorage of users
         return usnm.equals(username) && psswrd.equals(password);
+    }
+    public static String hashPassword(String password) {
+        // Hash the password with a work factor of 12
+        return BCrypt.hashpw(password, BCrypt.gensalt(12));
+
+    }
+    public static boolean verifyPassword(String password, String hashedPassword) {
+        // Verify the password against the hash
+        return BCrypt.checkpw(password, hashedPassword);
     }
 }
